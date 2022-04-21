2db160c05c161ac9bd01f3589d7d86f9acf32922
unknown <sony@mine.in>
2022-04-21 21:07:39 +0530

    js

---
+++ day-19/d19.js
@@ -0,0 +1,21 @@
+console.log("Working....");
+console.log("Hello World....!!");
+var number = 10;
+console.log(number);
+function Ex(n)
+{
+    console.log(n);
+    var a = n * n;
+    // console.log(a);
+    return a;
+    // console.log(a);
+
+}
+var newNumber = Ex(5); //op-1
+
+let a = 10;
+let b = 20;
+let add = a+b;          //op-2
+let subtract = a-b;     //op-3
+console.log(add);
+console.log(subtract);
