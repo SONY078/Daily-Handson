c8bd1212e1e5e2d3018e6a74a6bcb99c036e85d4
SONY078 <71254514+SONY078@users.noreply.github.com>
2022-04-22 19:44:27 +0530

    Add files via upload

---
+++ day-20/d20.js
@@ -0,0 +1,23 @@
+console.log("Working...");
+
+
+let fName = " sony ";
+let fname ;
+fname = "hiyyi";
+//let fname = "sunny";
+console.log(fname);
+console.log(fName);
+
+const lname = "V";
+console.log(lname);
+
+var named = " K ";
+console.log(named);
+var named = " J ";
+console.log(named);
+
+var pin = "1203";
+//console.log(pin);
+var pin;
+pin = 3495 ;
+console.log(pin);
