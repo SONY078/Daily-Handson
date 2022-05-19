eb0b609ba997664e436e9b1dfe901699b81aa94e
SONY078 <71254514+SONY078@users.noreply.github.com>
2022-05-19 20:16:27 +0530

    Add files via upload

---
+++ day-37/d37.js
@@ -0,0 +1,22 @@
+console.log("Working...");
+
+
+
+// $("button").click( function()
+// {
+//     $("p").hide();
+// })
+
+
+$("button").click( function()
+{
+    $("#ex").toggle();
+})
+
+
+
+// $("button").click( function()
+// {
+//     $("#sec").hide();
+// })
+
